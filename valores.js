2baf13cf445a5d5dd7465b437403067e5b7806fb
Jhon Chavez <jhchg7@gmail.com>
2020-09-01 21:07:03 -0400

    Elementos de un Lenguaje de Programación: Variables, Funciones y Sintaxis

---
+++ valores.js
@@ -0,0 +1,18 @@
+// valore primitivos 
+
+40  //valor numerico
+
+// "40" String ---> texto
+
+// valores boleanos
+true 
+false 
+
+// valores vacios 
+null
+undefined
+
+// valores no primitivos a arrays
+
+[1,2,3]
+{ nombre: "Diego"}
